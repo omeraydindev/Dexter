bcfca3ce95c4c61255ff75c8cd7116a4eb2178d1
Mike Anderson <heystudiosofficial@gmail.com>
2021-10-14 19:49:37 +0300

    test: Add basic tests for JADX

---
+++ app/src/test/kotlin/ma/dexter/tools/decompilers/jadx/JADXTests.kt
@@ -0,0 +1,43 @@
+package ma.dexter.tools.decompilers.jadx
+
+import com.google.common.truth.Truth.assertThat
+import ma.dexter.util.compile.Java2Dex
+import org.junit.Test
+
+class JADXTests {
+
+    @Test
+    fun `JADX decompile class basic`() {
+        val dexFile = Java2Dex.compile(
+            fileName = "Test.java",
+            javaCode = """
+                package test.aaa;
+                
+                public class Test {}
+            """
+        )
+
+        val result = JADXDecompiler().decompileDex("test.aaa.Test", dexFile)
+        assertThat(result).contains("Decompiled")
+        println(result)
+
+        val result2 = JADXDecompiler().decompileDex("test/aaa/Test", dexFile)
+        assertThat(result2).contains("Decompiled")
+        println(result2)
+    }
+
+    @Test
+    fun `JADX decompile class from defpackage`() {
+        val dexFile = Java2Dex.compile(
+            fileName = "Test.java",
+            javaCode = """
+                public class Test {}
+            """
+        )
+
+        val result = JADXDecompiler().decompileDex("Test", dexFile)
+        assertThat(result).contains("Decompiled")
+        println(result)
+    }
+
+}
