d10e7ea1c324091f448d18d13e67363229c93fdc
Mike Anderson <45513948+MikeAndrson@users.noreply.github.com>
2021-10-21 00:55:00 +0300

    feat: Add icons for classes in tree

--- app/src/main/kotlin/ma/dexter/ui/tree/dex/binder/DexItemNodeViewBinder.kt
+++ app/src/main/kotlin/ma/dexter/ui/tree/dex/binder/DexItemNodeViewBinder.kt
@@ -4,16 +4,20 @@ import android.view.View
 import ma.dexter.databinding.ItemDexTreeNodeBinding
 import ma.dexter.ui.tree.TreeNode
 import ma.dexter.ui.tree.base.BaseNodeViewBinder
+import ma.dexter.ui.tree.model.DexClassItem
 import ma.dexter.ui.tree.model.DexItem
 import ma.dexter.ui.util.dp
 import ma.dexter.ui.util.setMargins
+import ma.dexter.util.isAnnotation
+import ma.dexter.util.isEnum
+import ma.dexter.util.isInterface
 
-class DexItemNodeViewBinder (
+class DexItemNodeViewBinder(
     itemView: View,
     private val level: Int,
     private val toggleListener: DexItemNodeToggleListener,
     private val longClickListener: DexItemNodeLongClickListener
-): BaseNodeViewBinder<DexItem>(itemView) {
+) : BaseNodeViewBinder<DexItem>(itemView) {
 
     private lateinit var binding: ItemDexTreeNodeBinding
 
@@ -23,22 +27,53 @@ class DexItemNodeViewBinder (
         binding.root.setMargins(left = level * 20.dp)
         binding.title.text = treeNode.value.path
 
-        binding.icon.rotation = if (treeNode.isExpanded) 90F else 0F
+        binding.icExpand.rotation = if (treeNode.isExpanded) 90F else 0F
 
-        binding.icon.visibility =
-            if (treeNode.isLeaf) View.INVISIBLE else View.VISIBLE
+        val item = treeNode.value
+        if (item is DexClassItem) {
+            binding.icExpand.visibility = View.GONE
+            binding.cvClassDef.visibility = View.VISIBLE
+
+            binding.txClassDef.text = when {
+                item.classDef.isAnnotation() -> {
+                    binding.cvClassDef.setCardBackgroundColor(COLOR_INTERFACE)
+                    "@"
+                }
+                item.classDef.isEnum() -> {
+                    binding.cvClassDef.setCardBackgroundColor(COLOR_CLASS)
+                    "E"
+                }
+                item.classDef.isInterface() -> {
+                    binding.cvClassDef.setCardBackgroundColor(COLOR_INTERFACE)
+                    "I"
+                }
+                else -> { // Class
+                    binding.cvClassDef.setCardBackgroundColor(COLOR_CLASS)
+                    "C"
+                }
+            }
+        } else {
+            binding.icExpand.visibility = View.VISIBLE
+            binding.cvClassDef.visibility = View.GONE
+        }
     }
 
     override fun onNodeToggled(treeNode: TreeNode<DexItem>, expand: Boolean) {
-        binding.icon.animate()
-            .rotation(if (expand) 90F else 0F)
-            .setDuration(150)
-            .start()
+        if (binding.icExpand.visibility == View.VISIBLE) {
+            binding.icExpand.animate()
+                .rotation(if (expand) 90F else 0F)
+                .setDuration(150)
+                .start()
+        }
 
         toggleListener.onNodeToggled(treeNode)
     }
 
-    override fun onNodeLongClicked(view: View, treeNode: TreeNode<DexItem>, expanded: Boolean): Boolean {
+    override fun onNodeLongClicked(
+        view: View,
+        treeNode: TreeNode<DexItem>,
+        expanded: Boolean
+    ): Boolean {
         return longClickListener.onNodeLongClicked(view, treeNode)
     }
 
@@ -48,5 +83,10 @@ class DexItemNodeViewBinder (
 
     fun interface DexItemNodeLongClickListener {
         fun onNodeLongClicked(view: View, treeNode: TreeNode<DexItem>): Boolean
+    }
+
+    companion object {
+        const val COLOR_CLASS = 0xff67bed9.toInt()
+        const val COLOR_INTERFACE = 0xff487c39.toInt()
     }
 }
--- app/src/main/kotlin/ma/dexter/util/DexUtils.kt
+++ app/src/main/kotlin/ma/dexter/util/DexUtils.kt
@@ -1,7 +1,22 @@
 package ma.dexter.util
 
+import org.jf.dexlib2.AccessFlags
+import org.jf.dexlib2.iface.ClassDef
+
 fun isValidDexFileName(
     fileName: String
 ): Boolean {
     return fileName.startsWith("classes") && fileName.endsWith(".dex")
+}
+
+fun ClassDef.isInterface(): Boolean {
+    return (this.accessFlags and AccessFlags.INTERFACE.value) != 0
+}
+
+fun ClassDef.isEnum(): Boolean {
+    return (this.accessFlags and AccessFlags.ENUM.value) != 0
+}
+
+fun ClassDef.isAnnotation(): Boolean {
+    return (this.accessFlags and AccessFlags.ANNOTATION.value) != 0
 }
